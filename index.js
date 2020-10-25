28df620e1f26a0af9a0a8795e1225a15a9caa07b
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-10-25 16:03:23 +0100

    added factorial2 function that factorializes numbers with using a for loop

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Factorial function
+// Factorial function - factorialize numbers
 const factorial = (n) => {
     if (n < 0) {return -1}
     else if (n === 0) {return 1}
@@ -6,5 +6,18 @@ const factorial = (n) => {
         return n * factorial(n-1);
     }
 }
-  
-  console.log(factorial(6))
+
+const factorial2 = (n) => {
+    if (n < 0) {return -1}
+    else if (n === 0) {return 1}
+    else {
+        let result;
+        for (let i = 1; i <= n; i++) {
+            result ? result *= i : result = i;
+        }
+        return result;
+    }
+}
+
+
+
