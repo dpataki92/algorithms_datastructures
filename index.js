3d8996339bbcf64a2e416dc426e155a17723fe7a
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-11-23 20:26:31 +0100

    add mergeSort

--- index.js
+++ index.js
@@ -557,3 +557,27 @@ function selectionSort(array){
 
     return sortedArray;
 }
+
+// mergeSort - splits array into one element subarray and then merges them into one sorted array
+function merge(arr1, arr2) {
+    let sorted = [];
+
+    while (arr1.length !== 0 && arr2.length !== 0) {
+        if (arr1[0] < arr2[0]) {sorted.push(arr1.shift())}
+        else {sorted.push(arr2.shift())}
+    }
+
+    return sorted.concat(arr1).concat(arr2);
+}
+
+function mergeSort(arr) {
+    let midPoint = arr.length / 2;
+    let firstHalf = arr.slice(0, midPoint);
+    let secondHalf = arr.slice(midPoint, arr.length);
+
+    if (arr.length < 2) {
+        return arr;
+    } else {
+        return merge(mergeSort(firstHalf), mergeSort(secondHalf));
+    }
+}
