14b2164bc208ba8bb3f08f3fe3125cedf7d421f3
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-11-25 22:13:16 +0100

    add sum function

--- algorithms.js
+++ algorithms.js
@@ -642,3 +642,20 @@ function isBalanced(str) {
 
     return arr.length === 0;
 }
+
+// sum - Write a sum method which will work properly when invoked using either syntax below.
+
+console.log(sum(2,3));   // Outputs 5
+console.log(sum(2)(3));  // Outputs 5
+
+function sum(...args) {
+    if (arguments.length > 1) {
+        return args[0] + args[1];
+    } else {
+        return function(num) {
+            return num + args[0];
+        }
+    }
+}
+
+// 
