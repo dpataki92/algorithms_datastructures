e654caf70745291d2a0965b04bd670f4138a9043
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-12-17 18:27:25 +0000

    add Queue example to the data structure

--- data_structures.js
+++ data_structures.js
@@ -283,3 +283,24 @@ const findMiddle = linkedList => {
   }
   return slowPointer;
 };
+
+// QUEUE
+class Queue {
+  constructor() {
+    this.queue = new LinkedList();
+    this.size = 0;
+  }
+
+  enqueue(data) {
+    this.queue.addToTail(data);
+    this.size++;
+    console.log(`Added ${data} to queue! Queue size is now ${this.size}.`);
+  }
+
+  dequeue() {
+    const data = this.queue.removeHead();
+    this.size--;
+    console.log(`Removed ${data} from queue! Queue size is now ${this.size}.`);
+    return data;
+  }
+}
