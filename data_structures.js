2427645fa6384518eeabb4a0fb2452963953cd29
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-12-17 23:04:07 +0000

    add stack example to data structures

--- data_structures.js
+++ data_structures.js
@@ -323,3 +323,50 @@ class Queue {
     }
   }
 }
+
+// STACK
+class Stack {
+  constructor(maxSize = Infinity) {
+    this.stack = new LinkedList();
+    this.maxSize = maxSize;
+    this.size = 0;
+  }
+
+  hasRoom() {
+    return this.size < this.maxSize;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  push(value) {
+    if (this.hasRoom()) {
+      this.stack.addToHead(value);
+      this.size++;
+    }
+    else {
+      throw Error('Stack is full');
+    }
+  }
+
+  pop() {
+    if (!this.isEmpty()) {
+      const value = this.stack.removeHead();
+      this.size--;
+      return value;
+    }
+    else {
+      console.log('Stack is empty!');
+    }
+  }
+
+  peek() {
+    if (!this.isEmpty()) {
+      return this.stack.head.data;
+    } 
+    else {
+      return null;
+    }
+  }
+}
