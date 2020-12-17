9d4740fc0f7c94e90a9997758d3d1b4fbdab441c
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-12-17 16:18:34 +0000

    add function to swap nodes in a linked list

--- data_structures.js
+++ data_structures.js
@@ -202,3 +202,42 @@ class Node {
   }
 }
 
+// Swapping elements in a linked list
+function swapNodes(list, data1, data2) {
+  let node1 = list.head;
+  let node2 = list.head;
+  let node1Prev = null;
+  let node2Prev = null;
+ 
+  while (node1 !== null) {
+    if (node1.data === data1) {
+      break;
+    }
+    node1Prev = node1;
+    node1 = node1.getNextNode();
+  }
+
+  while (node2 !== null) {
+    if (node2.data === data2) {
+      break;
+    }
+    node2Prev = node2;
+    node2 = node2.getNextNode();
+  }
+
+  if (node1Prev === null) {
+    list.head = node2;
+  } else {
+    node1Prev.setNextNode(node2);
+  }
+
+  if (node2Prev === null) {
+    list.head = node1;
+  } else {
+    node2Prev.setNextNode(node1);
+  }
+
+  let temp = node1.getNextNode();
+  node1.setNextNode(node2.getNextNode());
+  node2.setNextNode(temp); 
+}
