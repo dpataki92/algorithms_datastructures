cbf95ff1f3125f67e3c9cdd8c32ee8931ed8e39a
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-12-17 19:27:44 +0000

    update queue example to handle overflow and underflow

--- data_structures.js
+++ data_structures.js
@@ -286,21 +286,40 @@ const findMiddle = linkedList => {
 
 // QUEUE
 class Queue {
-  constructor() {
+  constructor(maxSize = Infinity) {
     this.queue = new LinkedList();
+    this.maxSize = maxSize;
     this.size = 0;
   }
 
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  hasRoom() {
+    return this.size < this.maxSize;
+  }
+
   enqueue(data) {
-    this.queue.addToTail(data);
-    this.size++;
-    console.log(`Added ${data} to queue! Queue size is now ${this.size}.`);
+    if (this.hasRoom()) {
+          this.queue.addToTail(data);
+          this.size++;
+          console.log(`Added ${data} to queue! Queue size is now ${this.size}.`);
+    } 
+    else{
+      throw Error('Queue is full!');
+    }
   }
 
   dequeue() {
-    const data = this.queue.removeHead();
-    this.size--;
-    console.log(`Removed ${data} from queue! Queue size is now ${this.size}.`);
-    return data;
+    if (!this.isEmpty()) {
+      const data = this.queue.removeHead();
+      this.size--;
+      console.log(`Removed ${data} from queue! Queue size is now ${this.size}.`);
+      return data;
+    } 
+    else {
+      throw new Error("Queue is empty!");
+    }
   }
 }
