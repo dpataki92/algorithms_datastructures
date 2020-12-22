a8a2935b8d7e3091878aee5ab747d4439aff2fc2
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-12-22 17:44:19 +0000

    add initial HashMap example to data structures

--- data_structures.js
+++ data_structures.js
@@ -370,3 +370,28 @@ class Stack {
     }
   }
 }
+
+// Hash map
+class HashMap {
+  constructor(size = 0) {
+    this.hashmap = new Array(size);
+  }
+
+  hash(key) {
+    let hashCode = 0;
+    for (let i = 0; i < key.length; i++) {
+      hashCode += hashCode + key.charCodeAt(i);
+    }
+    return hashCode % this.hashmap.length;
+  }
+
+  assign(key, value) {
+    const arrayIndex = this.hash(key);
+    this.hashmap[arrayIndex] = value;
+  }
+  
+  retrieve(key) {
+    const arrayIndex = this.hash(key);
+    return this.hashmap[arrayIndex];
+  }
+}
