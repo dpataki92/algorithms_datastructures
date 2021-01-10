5d41999492c489c4c6c3e96037a0f7157ab99a1f
Daniel Pataki <ptamasdaniel92@gmail.com>
2021-01-10 17:53:49 +0100

    add quickSort function

--- algorithms.js
+++ algorithms.js
@@ -911,7 +911,7 @@ const binarySearch = (arr, target) => {
     const sortedArray = [];
 
     while (leftArray.length > 0 && rightArray.length > 0) {
-        
+
       if (leftArray[0] < rightArray[0]) {
         sortedArray.push(leftArray.shift());
       } else {
@@ -920,4 +920,32 @@ const binarySearch = (arr, target) => {
     }
     
     return sortedArray.concat(leftArray).concat(rightArray);
+  }
+
+  // quickSort - O(n * log n) 
+  const quickSort = (array, leftBound = 0, rightBound = array.length - 1) => {
+    if (leftBound < rightBound) {
+      const pivotIndex = partition(array, leftBound, rightBound);
+      quickSort(array, leftBound, pivotIndex - 1);
+      quickSort(array, pivotIndex, rightBound);
+    }
+    return array;
+  }
+  
+  const partition = (array, leftIndex, rightIndex) => {
+    const pivot = array[Math.floor((rightIndex + leftIndex) / 2)];
+    while (leftIndex <= rightIndex) {
+      while (array[leftIndex] < pivot) {
+        leftIndex++;
+      }
+      while (array[rightIndex] > pivot) {
+        rightIndex--;
+      }
+      if (leftIndex <= rightIndex) {
+        swap(array, leftIndex, rightIndex);
+        leftIndex++;
+        rightIndex--;
+      }
+    }
+    return leftIndex;
   }
