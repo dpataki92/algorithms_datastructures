98608287263e8682a0f294ee45258db207c96f3e
Daniel Pataki <ptamasdaniel92@gmail.com>
2021-01-10 15:30:18 +0100

    add second mergeSort implementation

--- algorithms.js
+++ algorithms.js
@@ -891,3 +891,33 @@ const binarySearch = (arr, target) => {
     arr[indexTwo] = arr[indexOne];
     arr[indexOne] = temp;
   };
+
+  //mergeSort2 - O(n * log(n))
+  const mergeSort2 = (startArray) => {
+    const length = startArray.length;
+
+    if (length === 1) {
+      return startArray;
+    }
+    
+    const mid = Math.floor(length / 2);
+    const leftArray = startArray.slice(0, mid);
+    const rightArray = startArray.slice(mid, length);
+  
+    return merge(mergeSort(leftArray), mergeSort(rightArray))
+  }
+  
+  const merge = (leftArray, rightArray) => {
+    const sortedArray = [];
+
+    while (leftArray.length > 0 && rightArray.length > 0) {
+        
+      if (leftArray[0] < rightArray[0]) {
+        sortedArray.push(leftArray.shift());
+      } else {
+        sortedArray.push(rightArray.shift());
+      }
+    }
+    
+    return sortedArray.concat(leftArray).concat(rightArray);
+  }
