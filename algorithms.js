d40070ae6f191e32ae7e4395b05105191447bab7
Daniel Pataki <ptamasdaniel92@gmail.com>
2021-01-19 22:12:11 +0100

    add capturingRainwater function

--- algorithms.js
+++ algorithms.js
@@ -980,3 +980,25 @@ const binarySearch = (arr, target) => {
       return primes
     }, []);
   }
+
+  // capturingRainwater - returns sum of water units captured after raining
+  function capturingRainwater(heights) {
+    let totalWater = 0;
+    let leftPointer = 0;
+    let rightPointer = heights.length - 1;
+    let leftBound = 0;
+    let rightBound = 0;
+  
+   while (leftPointer < rightPointer) {
+      if (heights[leftPointer] <= heights[rightPointer]) {
+        leftBound = Math.max(heights[leftPointer], leftBound);
+        totalWater += leftBound - heights[leftPointer];
+        leftPointer++;
+      } else {
+        rightBound = Math.max(heights[rightPointer], rightBound);
+        totalWater += rightBound - heights[rightPointer];
+        rightPointer--;
+      }
+    }
+    return totalWater;
+  }
